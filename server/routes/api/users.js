6c16d80910c4703d98df2ae914af36c6efbdf7b2
Forl <kumaloveslife@gmail.com>
2019-08-12 21:26:26 +0800

    添加login路由

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -12,7 +12,7 @@ router.get('/test', (req, res) => {
   res.json({msg:'login works'})
 })
 
-// $route   GET api/users/register
+// $route   POST api/users/register
 // @desc    返回请求的json数据
 // @access  public
 router.post('/register', (req, res) => {
@@ -44,6 +44,31 @@ router.post('/register', (req, res) => {
         }
       })
       .catch( err => console.log(err))
+})
+
+// $route   POST api/users/login
+// @desc    返回token
+// @access  public
+router.post('/login', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  //查询数据
+  User.findOne({email})
+      .then(user => {
+        if(!user) {
+          return res.status(404).json({email: '用户不存在'})
+        };
+
+        // 密码匹配
+        bcrypt.compare(password, user.password)
+              .then(isMatch => {
+                if(isMatch) {
+                  res.json({msg: 'success'})
+                }else {
+                  return res.status(400).json({password: '密码错误'})
+                }
+              })
+      })
 })
 
 module.exports = router
