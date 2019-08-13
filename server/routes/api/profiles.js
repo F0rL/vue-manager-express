0ea47c02a2fb8d1ada1ece8b21a58374bc21fd45
Forl <kumaloveslife@gmail.com>
2019-08-13 20:02:47 +0800

    添加更新、删除接口

--- server/routes/api/profiles.js
+++ server/routes/api/profiles.js
@@ -28,6 +28,37 @@ router.post('/add', password.authenticate('jwt', {session: false}), (req, res) =
   }).catch(err => res.json(err))
 })
 
+// $route   POST api/profiles/edit/:id
+// @desc    编辑信息
+// @access  private
+router.post('/edit/:id', password.authenticate('jwt', {session: false}), (req, res) => {
+  const profileFields = {}
+  if(req.body.type) profileFields.type = req.body.type
+  if(req.body.describe) profileFields.describe = req.body.describe
+  if(req.body.income) profileFields.income = req.body.income
+  if(req.body.expend) profileFields.expend = req.body.expend
+  if(req.body.cash) profileFields.cash = req.body.cash
+  if(req.body.remark) profileFields.remark = req.body.remark
+
+  Profile.findOneAndUpdate(
+      {_id: req.params.id},
+      {$set: profileFields},
+      {new: true}
+    ).then(profile => res.json(profile))
+     .catch(err => res.json(err))
+})
+
+// $route   delete api/profiles/delete/:id
+// @desc    删除信息
+// @access  private
+router.delete('/delete/:id', password.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOneAndRemove({_id: req.params.id})
+    .then(profile => {
+      profile.save().then(profile => res.json(profile))
+    })
+    .catch(error => res.status(404).json(`删除失败，原因：${error.message}`))
+})
+
 // $route  GET api/profiles
 // @desc    获取所有信息
 // @access  private
@@ -39,7 +70,7 @@ router.get('/', password.authenticate('jwt', {session: false}), (req, res) => {
     }
     res.json(profile)
   })
-  .catch(err => res.status(404).json(err))
+  .catch(error => res.status(404).json(error))
 })
 
 // $route  GET api/profiles/:id
